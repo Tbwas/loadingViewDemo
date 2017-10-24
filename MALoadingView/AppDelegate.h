9a317a33e6ffa7fae1109827d66d1087372847cf
Tbwas <15668995961@163.com>
2017-10-24 23:42:19 +0800

    Initial Commit

---
+++ MALoadingView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MALoadingView
+//
+//  Created by xindong on 17/10/24.
+//  Copyright © 2017年 xindong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MALoadingView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MALoadingView
+//
+//  Created by xindong on 17/10/24.
+//  Copyright © 2017年 xindong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
